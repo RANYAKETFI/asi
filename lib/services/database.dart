d7182731aa39840f82119cc2e356900318755bd2
zeynebGasmi <34374317+zeynebGasmi@users.noreply.github.com>
2021-03-30 14:50:07 +0200

    Add files via upload

---
+++ lib/services/database.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_app/models/the.dart';
+
+class DatabaseService {
+  // collection reference
+  final CollectionReference theCollection =
+      Firestore.instance.collection('TheChezAbelkader');
+
+  List<The> _theListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return The(name: doc.data['name'] ?? '', prix: doc.data['prix'] ?? '0');
+    }).toList();
+  }
+
+  // get brews stream
+  Stream<List<The>> get thes {
+    return theCollection.snapshots().map(_theListFromSnapshot);
+  }
+}
