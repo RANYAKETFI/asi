e822d77a0038b519bb8949e53ceff2e59ebef572
zeynebGasmi <34374317+zeynebGasmi@users.noreply.github.com>
2021-03-30 14:46:03 +0200

    Add files via upload

---
+++ lib/screens/the_tile.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_app/models/the.dart';
+import 'package:flutter/material.dart';
+
+class TheTile extends StatelessWidget {
+  final The the;
+  TheTile({this.the});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Card(
+        margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 25.0,
+          ),
+          title: Text(the.name),
+          subtitle: Text('Price ${the.prix}'),
+        ),
+      ),
+    );
+  }
+}
